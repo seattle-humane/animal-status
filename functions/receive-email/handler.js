3e9e5789d0e90cb11d7a0e300963501be70cf8df
Dan Bjorge <dan@dbjorge.net>
2017-08-12 18:22:00 -0700

    Initialize documentDB client in handler

--- functions/receive-email/handler.js
+++ functions/receive-email/handler.js
@@ -2,10 +2,11 @@
 
 const awssdk = require('aws-sdk');
 const s3 = new awssdk.S3()
-const bucketName = 'animal-status-email';
+const s3BucketName = 'animal-status-email';
+const dynamoDocumentClient = new awssdk.DynamoDB.DocumentClient();
 
 const EmailReceiver = require('./email-receiver')
-const emailReceiver = new EmailReceiver(s3, bucketName)
+const emailReceiver = new EmailReceiver(s3, s3BucketName, dynamoDocumentClient)
 
 exports.handler = function (event, context, lambda_callback) {
     var sesNotification = event.Records[0].ses;
